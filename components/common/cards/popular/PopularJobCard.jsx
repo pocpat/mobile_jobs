9b5cdafe07d60160966e5c44d697f123ca4ab07b
Elena Kroupkin <pocoat@gmail.com>
2025-05-01 15:39:49 +1200

    change default company logo

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -12,13 +12,22 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
     
       style={styles.logoContainer(selectedJob, item)}
       onPress={ ()=> handleCardPress(item)}>
+     {/* // <Image
+      //   source={{ uri: checkImageURL(item.employer_logo)
+      //     ? item.employer_logo
+      //    : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
+      //    }}
+      //   resizeMode="contain"
+      //   style={styles.logoImage}/> */}
       <Image
-        source={{ uri: checkImageURL(item.employer_logo)
-          ? item.employer_logo
-          : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
-         }}
-        resizeMode="contain"
-        style={styles.logoImage}/>
+  source={
+    checkImageURL(item.employer_logo)
+      ? { uri: item.employer_logo }
+      : require('@/assets/icons/defaultCompanyLogo.png')
+  }
+  resizeMode="contain"
+  style={styles.logoImage}
+/>
     
     </TouchableOpacity>
 
